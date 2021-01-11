c28521f7f13554c0200d3efda2f174c9820cb742
Aldrick Rasquinha <aldrickrasquinha@gmail.com>
2021-01-11 23:54:22 +0530

    Desktop Commit

--- get/deleteMail.php
+++ get/deleteMail.php
@@ -4,5 +4,6 @@ $email=$_SESSION['email'];
 $sql=mysqli_connect("localhost","root","","EmailApp");
 $mails=$_GET['mailsTBD'];
 foreach ($mails as $mail){
-    $str="UPDATE `$email` SET `trash`=1 WHERE `id`=$mail";
+    $str="UPDATE `$email` SET `trash`=1 WHERE `id`=`$mail`";
+    mysqli_query($sql,$str);
 }
