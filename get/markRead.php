a7cbd459ea6940c40a40d95b8ac0e829d14fe948
Aldrick Rasquinha <aldrickrasquinha@gmail.com>
2021-01-14 10:44:29 +0530

    Desktop Commit

--- get/markRead.php
+++ get/markRead.php
@@ -4,6 +4,6 @@ $email=$_SESSION['email'];
 $sql=mysqli_connect("localhost","root","","EmailApp");
 $mails=json_decode(stripslashes($_GET['mailsTBD']));
 foreach ($mails as $mail){
-    $str="UPDATE `$email` SET `trash`=1 WHERE `id`=$mail";
+    $str="UPDATE `$email` SET `readflag`=1 WHERE `id`=$mail";
     mysqli_query($sql,$str);
 }
