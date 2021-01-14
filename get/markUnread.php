41bf75ff311e589db24fcc042d58d969dede4a52
Aldrick Rasquinha <aldrickrasquinha@gmail.com>
2021-01-14 11:32:23 +0530

    Desktop Commit

---
+++ get/markUnread.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+$email=$_SESSION['email'];
+$sql=mysqli_connect("localhost","root","","EmailApp");
+$mails=json_decode(stripslashes($_GET['mailsTBD']));
+foreach ($mails as $mail){
+    $str="UPDATE `$email` SET `readflag`=0 WHERE `id`=$mail";
+    mysqli_query($sql,$str);
+}
