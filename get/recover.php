0461283f0ec53d2603b09dd47cef35e4100fbfcc
Aldrick Rasquinha <aldrickrasquinha@gmail.com>
2021-01-17 23:07:09 +0530

    Desktop Commit

---
+++ get/recover.php
@@ -0,0 +1,33 @@
+<?php
+$sql=mysqli_connect("localhost","root","","EmailApp");
+$phone=$_GET['phone'];
+$str="SELECT * FROM users WHERE phone='$phone'";
+$res=mysqli_query($sql,$str);
+if(mysqli_num_rows($res)>0) {
+    $phone="91".$phone;
+    $phone=(int)$phone;
+    $data=mysqli_fetch_assoc($res);
+    $email=$data['email'];
+    $password=$data['pass'];
+    // Account details
+    $apiKey = urlencode('VzTOZJAKuY0-l9FtL7tT49dM4WYavAz55sdpIgbxgx');
+    // Message details
+    $numbers = array($phone);
+    $sender = urlencode('TXTLCL');
+    $message = rawurlencode("Your email is: '$email' and your password is: '$password'");
+    $numbers = implode(",", $numbers);
+    // Prepare data for POST request
+    $data = array('apikey' => $apiKey, 'numbers' => $numbers, 'sender' => $sender, 'message' => $message);
+    // Send the POST request with cURL
+    $ch = curl_init('https://api.textlocal.in/send/');
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($ch);
+    curl_close($ch);
+    // Process your response here
+    echo "Credentials sent to your registered phone number ".$response;
+}
+else{
+    echo "Invalid phone number";
+}
--- get/viewMail.php
+++ get/viewMail.php
@@ -6,4 +6,6 @@ $id=$_GET['id'];
 $str="SELECT * FROM `$email` WHERE `id`='$id'";
 $res=mysqli_query($sql,$str);
 $data=mysqli_fetch_assoc($res);
+$str="UPDATE `$email` SET `readflag`=1 WHERE `id`=$id";
+mysqli_query($sql,$str);
 echo json_encode($data);
